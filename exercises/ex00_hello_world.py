7caa23e01f510b2de25db867d59b4c9e8c21fee0
Jacob Rogers <jacobtr@ad.unc.edu>
2021-08-28 16:28:42 -0400

    finished ex01

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,5 +1,5 @@
 """My first program for COMP110."""
 
-_author_ = "730403482"
+__author__ = "730403482"
 
-print("Hello, small world.")
+print("Hello, world.")
--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,9 @@
 # TODO: Write docstring here
-
+"""Hyping myself up!"""
 # TODO: Initialize __author__ variable here
-
+_author_: str = str(730403482)
 # TODO: Implement your program logic here
+name: str = input("What is your name? ")
+print("You're the man " + name + "!")
+print(name + " is super smart and devishly handsome.")
+print("Keep rocking " + name + ", and always be yourself!")

912041c7f11e671669ff21bb4d457808aedad5fd
Jacob Rogers <jacobtr@ad.unc.edu>
2021-08-28 16:35:33 -0400

    corrected ex01

--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -1,7 +1,7 @@
 """Practicing with relational operators."""
-_author_: str= str(730403482)
-left_hand_side: int= int(input("Left-Hand side: "))
-right_hand_side: int= int(input("Right-Hand side: "))
+_author_: str = str(730403482)
+left_hand_side: int = int(input("Left-Hand side: "))
+right_hand_side: int = int(input("Right-Hand side: "))
 print(str(left_hand_side) + " < " + str(right_hand_side) + " is " + str(left_hand_side < right_hand_side))
 print(str(left_hand_side) + " >= " + str(right_hand_side) + " is " + str(left_hand_side >= right_hand_side))
 print(str(left_hand_side) + " == " + str(right_hand_side) + " is " + str(left_hand_side == right_hand_side))
